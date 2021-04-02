d4ffbab511298f50a20a3151edc15cb868c9a4c6
Ethan Belcher <65253767+SmokeyAndFriends@users.noreply.github.com>
2021-04-02 13:52:49 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 // Data
-const loc = document.getElementById('loc');
+const clicks = document.getElementById('loc');
 const img = document.getElementById('cImg');
 
-var clicks = 0;
+var totalClicks = 0;
 
 // Event listeners
 img.addEventListener("click", function() {
-  clicks += 1;
-  loc.textContent = "Lines of code: " + clicks;
+  totalClicks += 1;
+  clicks.textContent = "Clicks: " + totalClicks;
 });
