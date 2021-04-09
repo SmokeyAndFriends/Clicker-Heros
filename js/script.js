248c1b98260a27bb6e2eb894004bdc48ee634cc1
Ethan Belcher <pandurrerock@gmail.com>
2021-04-09 14:33:12 +0000

    yessir

--- js/script.js
+++ js/script.js
@@ -4,12 +4,27 @@ const img = document.getElementById('cImg');
 
 var totalClicks = 0;
 var cps = 0;
+var wipe = NaN;
 
 // functions
 function loadData() {
    totalClicks = JSON.parse(localStorage.getItem('clicks'));
    clicks.textContent = "Clicks: " + totalClicks;
-}
+};
+
+function wipeData() {
+  wipe = prompt("WARNING THIS WILL WIPE YOUR DATA(Including Total Clicks). ARE YOU SURE? [y/n]")
+  switch(wipe) {
+    case("y"): {
+      totalClicks = localStorage.removeItem('clicks');
+      clicks.textContent = "clicks" + totalClicks;
+    }
+    case("n"): {
+      return;
+    }
+    default: {NaN}
+  };
+};
 
 // Event listeners
 img.addEventListener("click", function() {
